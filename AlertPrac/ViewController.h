7555dcc74649faf103cab7024946fe454c44de65
cpromise <cpromise1@naver.com>
2015-09-30 23:59:32 +0900

    Initial Commit

---
+++ AlertPrac/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AlertPrac
+//
+//  Created by SH on 2015. 9. 30..
+//  Copyright © 2015년 SH. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ AlertPrac/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  AlertPrac
+//
+//  Created by SH on 2015. 9. 30..
+//  Copyright © 2015년 SH. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
